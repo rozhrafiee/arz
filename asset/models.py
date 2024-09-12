e9dbd6ff2ea8cfabadd9e0c510222969cdf048ad
mahan <sepanloooo@gmail.com>
2024-09-12 12:24:55 +0330

    mahan1

--- asset/models.py
+++ asset/models.py
@@ -1,20 +1,13 @@
 from django.db import models
 
-class Currrency(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.CharField(max_length=100)
-    dayChange = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
 
 class Allarm(models.Model):
-    name = models.ForeignKey(Currrency,on_delete=models.CASCADE)
+    name = models.CharField(max_length=1000)
     price = models.CharField(max_length=1000)
     up_or_downe = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name.name
+        return self.name
 
     
 
